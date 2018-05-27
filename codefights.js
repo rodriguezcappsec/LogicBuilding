91cda2d23e7ce47ec126d9b86b94c4f1933fead1
Luis <luis.rodriguezcastro31@gmail.com>
2018-05-27 14:08:07 -0700

    new codefights code

--- codefights.js
+++ codefights.js
@@ -1,18 +1,38 @@
-let grid = [
-    ['.', '.', '.', '1', '4', '.', '.', '2', '.'],
-    ['.', '.', '6', '.', '.', '.', '.', '.', '.'],
-    ['.', '.', '.', '.', '.', '.', '.', '.', '.'],
-    ['.', '.', '1', '.', '.', '.', '.', '.', '.'],
-    ['.', '6', '7', '.', '.', '.', '.', '.', '9'],
-    ['.', '.', '.', '.', '.', '.', '8', '1', '.'],
-    ['.', '3', '.', '.', '.', '.', '.', '.', '6'],
-    ['.', '.', '.', '.', '.', '7', '.', '.', '.'],
-    ['.', '.', '.', '5', '.', '.', '.', '7', '.']
-]
+let matrixElementsSum = (matrix) => {
 
-let x = 1;
-let y = 0;
-for (var index = 0; index < grid.length; index++) {
+    var position = 0;
+    let saver = [];
+    let array = [];
+    for (var value = 0; value < matrix.length; ++value) {
 
+        saver.push(matrix[value][position]);
+
+        if (value == 2) {
+            (saver[0] == 0) ? (saver[0] = 0, saver[1] = 0, saver[2] = 0) : saver;
+            (saver[1] == 0) ? (saver[2] = 0) : saver;
+            array.push(saver), saver = [], value = -1, ++position;
+        }
+        if (position == matrix[0].length) {
+            var sum = 0;
+            var newArray = array.toString().split(",").join("").split("0").join("");
+            for (let index = 0; index < newArray.length; index++) {
+                sum += Number(newArray[index]);
+            }
+            break;
+        }
+
+    }
+    if (matrix.length == 1) {
+        var _sum = 0;
+        var justOne = matrix.toString().split(",").join("");
+        for (let index = 0; index < justOne.length; index++) {
+            _sum += Number(justOne[index]);
+        }
+        return _sum;
+    }
+    if (sum == null) {
+        return 0;
+    }
+    return sum;
 
 }
